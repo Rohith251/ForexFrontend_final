8d80c27412a8b2daaaeb365377d85a216a682369
Rohith251 <nukalarohith26@gmail.com>
2025-05-06 12:57:02 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8081/', // your backend URL
+  baseURL: 'http://localhost:8082/', // your backend URL
 });
 
 // Automatically attach JWT token to every request if available
